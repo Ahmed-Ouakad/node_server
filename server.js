3dd885e00fb22421e243ab56dfce3f5e3448a4ea
Ahmed-Ouakad <60930237+Ahmed-Ouakad@users.noreply.github.com>
2025-02-02 15:22:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000; // Use Railway's PORT
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! 🚀 Your server is running!");
+  res.send("Hello, World! 🚀 Your server is running! it's from ahmed");
 });
 
 app.listen(PORT, () => {
