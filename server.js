aca4ea9d73b27368c67d563dd55b8af5a8991b40
Ahmed-Ouakad <60930237+Ahmed-Ouakad@users.noreply.github.com>
2025-02-04 12:24:00 +0100

    Update 2 server.js

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ const PORT = process.env.PORT || 3000; // Use Railway's PORT
 app.get("/", (req, res) => {
   res.send("Hello, World! 🚀 Your server is running! it's from ahmed");
 });
+app.post("/echo", (req, res) => {
+  const { from, to } = req.body;
 
+  if (!from || !to) {
+    return res.status(400).json({ error: "Both 'from' and 'to' fields are required!" });
+  }
+
+  res.json({
+    message: "Data received successfully!",
+    from: from,
+    to: to,
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
